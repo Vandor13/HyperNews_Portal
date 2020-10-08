2abe22156b7c8f3e361a62ce05a1e723c9593d0d
joerg.brose <Dummgit0>
2020-10-08 16:11:40 +0200

    Completed 2/ 5 Stages

--- HyperNews Portal/task/hypernews/news/urls.py
+++ HyperNews Portal/task/hypernews/news/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, re_path
 from . import views
 from .views import NewsView
 
-urlpatterns = [path("", NewsView.as_view()),
+urlpatterns = [path("", views.home, name="home"),
                re_path("news/(?P<news_id>[^/]*)/?", NewsView.as_view()),
                ]
--- HyperNews Portal/task/hypernews/news/views.py
+++ HyperNews Portal/task/hypernews/news/views.py
@@ -1,16 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from django.conf import settings
+import json
+import os
 
 # Create your views here.
 
+
 class NewsView(View):
-    def get(self, request, news_id=0, *args, **kwargs):
+    def get(self, request, news_id=13, *args, **kwargs):
+        # print("Hello")
+        # file_name = str(os.path.dirname(os.path.abspath(__file__)))+ "\\" + settings.NEWS_JSON_PATH
+        file_name = settings.NEWS_JSON_PATH
+        # print(file_name)
+        print(news_id)
+        news_article = None
+        if news_id:
+            with open(file_name) as file:
+                news = json.load(file)
+                # print("Length of read json: " + str(len(news)))
+                news_article = None
+                news_article = [article for article in news if int(article["link"]) == int(news_id)]
+            # print("News id is: " + str(news_id))
+        # print("Length of news article: " + str(len(news_article)))
+        if news_article and len(news_article) > 0:
+            context = {"news": news_article[0]}
+        else:
+            context = {"news": "No news yet", "text": "No news article found"}
         return render(
-            request, 'news/news.html'
+            request, 'news/news.html', context=context
         )
 
+
 def home(request):
-    def get(self, get_request, *args, **kwargs):
-        # context = {"news_id": news_id}
-        return "Coming soon"
+    return HttpResponse("Coming soon")
