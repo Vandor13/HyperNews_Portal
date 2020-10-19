ec722d9fb798c3aa65dca530b551ab6f8386f61f
joerg.brose <Dummgit0>
2020-10-19 18:10:37 +0200

    All Stages Complete + Style Sheet!

--- HyperNews Portal/task/hypernews/news/urls.py
+++ HyperNews Portal/task/hypernews/news/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, re_path
 from . import views
 from .views import NewsView, AllNewsView, CreateNewsView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [path("", views.home, name="home"),
                path("news/", AllNewsView.as_view()),
                path("news/create/", CreateNewsView.as_view()),
                re_path("news/(?P<news_id>[^/]*)/?", NewsView.as_view()),
                ]
+
+urlpatterns += static(settings.STATIC_URL)
